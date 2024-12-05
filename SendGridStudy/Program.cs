0d0068153e353da75a6f1e43b3e88c52f8f694bf
Felipe Pinheiro <t_felipe.pinheiro@hapvida.com.br>
2024-12-05 10:22:56 -0300

    Add project files.

---
+++ SendGridStudy/Program.cs
@@ -0,0 +1,32 @@
+﻿// using SendGrid's C# Library
+// https://github.com/sendgrid/sendgrid-csharp
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace SendGridStudy
+{
+    internal class Example
+    {
+        private static void Main()
+        {
+            Execute().Wait();
+        }
+
+        static async Task Execute()
+        {
+            var apiKey = "api key";
+            var client = new SendGridClient(apiKey);
+            var to = new EmailAddress("test@test.com", "test test");
+            var from = new EmailAddress("test@test.com", "test test");
+            var templateId = "Unique template Id";
+
+            var data = new { Name = "test" };
+
+            var message = MailHelper.CreateSingleTemplateEmail(from, to, templateId, data);
+
+            await client.SendEmailAsync(message);
+        }
+    }
+}
+
+
